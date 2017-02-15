bce7d11c5aefa2bdd7b89f0ed788173c7544cf15
mario7746 <m.kennedykavouras@gmail.com>
2017-02-15 11:51:26 -0500

    corrected development port, added event handlers

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     production: 'https://git.heroku.com/elimination-health-api.git',
-    development: 'http://localhost:7165',
+    development: 'http://localhost:4741',
   },
 };
 
